ac3d411c933abf160e11830cfe7a5d7dc4ba5778
SergeyK <sav1575@mail.ru>
2023-11-27 15:16:58 +0300

    Task_33

--- Task27.py
+++ Task27.py
@@ -10,15 +10,23 @@
 # shore shells
 # Output: 13
 
-text = "She sells sea shells on the sea shore The shells\
- that she sells are sea shells I'm sure. So if she sells sea\
- shells on the sea shore I'm sure that the shells are sea\
- shore shells"
+# text = "She sells sea shells on the sea shore The shells\
+#  that she sells are sea shells I'm sure. So if she sells sea\
+#  shells on the sea shore I'm sure that the shells are sea\
+#  shore shells"
 
-words = text.lower().replace('.', ' ').split()
+# words = text.lower().replace('.', ' ').split()
 
-st = set(words)
+# st = set(words)
 
-print(len(st))
+# print(len(st))
 
 # print(len(set(words)))
+
+list_1 = [1, 2, 8, 8, 2, 2, 4, 5, 10, 9, 9, 5,]
+k = 3
+count_k = 0
+for i in list_1:
+    if i == k:
+        count_k += 1
+print(count_k)
