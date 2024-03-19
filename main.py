f0d1bb1fd5eec4840038e75ebfb4e9ef24e5b783
SergeyK <sav1575@mail.ru>
2024-03-19 11:32:11 +0300

    Задание

--- main.py
+++ main.py
@@ -2,12 +2,25 @@
 # res = (1 + 2 + 3)
 # print(res)
 
-n = 123
-n = int(input())
-a = n // 100
-b = n % 100 // 10
-c = n % 10
-# Введите ваше решение ниже
+# n = 123
+# # n = int(input())
+# a = n // 100
+# b = n % 100 // 10
+# c = n % 10
+# # Введите ваше решение ниже
 
-res = (n//100 + n%100//10 + n%10)
-print(res)
+# res = (n//100 + n%100//10 + n%10)
+# print(res)
+
+# 
+def sum_nam(a, b):
+    sum = a + b
+    print(sum)
+    return sum
+
+
+
+print(sum_nam(sum_nam(5, 32), 43))
+
+sum_nam(5, 32)
+sum_nam(10, 52)
